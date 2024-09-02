e70ca40ed63ce4432587a0b13edf98315ed3285d
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2024-09-02 22:22:28 +0530

    first commit

---
+++ src/testing/Testing.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("so now in VS Code java is working ");
+        System.out.println("javac src/testing/Testing.java");
+        System.out.println("java -cp src testing.Testing");
+    }
+
+}
