123fa39a3d46b0d604a0da12dc9cf766db6c71be
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2024-09-02 22:25:05 +0530

    Adding second commit

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -6,6 +6,7 @@ public class Testing {
         System.out.println("so now in VS Code java is working ");
         System.out.println("javac src/testing/Testing.java");
         System.out.println("java -cp src testing.Testing");
+        System.out.println("Testing again after push command");
     }
 
 }
